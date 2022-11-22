89151e3c2e5e5e4b5331a666f47e258a9ba0d9db
Daniel See <daniel.see@streetec.net>
2022-11-22 12:55:03 +0100

    initial

---
+++ ImageTest/ImageTest.iOS/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+using ObjCRuntime;
+
+namespace ImageTest.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            var img = UIImage.FromBundle("car_settings");
+            System.Diagnostics.Debug.WriteLine($"image {(img == null ? "is" : "is not")} null");
+            var vector = UIImage.FromBundle("car_settings_vector");
+            System.Diagnostics.Debug.WriteLine($"vector {(vector == null ? "is" : "is not")} null");
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
